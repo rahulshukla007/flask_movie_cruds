76bf7bc99de094559800e0d72102bdc399c336a2
RahulShukla <rahultest@gmail.com>
2022-07-30 16:44:45 +0530

    added db file

--- mymovie_db.py
+++ mymovie_db.py
@@ -3,12 +3,12 @@ import psycopg2
 
 conn = psycopg2.connect(
 
-        host        = 'localhost',
-        database    = "mymovie_db",
-        user        = "postgres",
-        password    = '461775'
+        host        = "ec2-34-235-31-124.compute-1.amazonaws.com",
+        database    = "d87b79k4h2v8l",
+        user        = "tmzuihdcumyntq",
+        password    = "4429cf39d83cb78718a7b9e5f7e623835ae603c26e7d187bb15d300be0d73790",
+        port        = "5432"
         )
-
 cur = conn.cursor()
 
 cur.execute("CREATE TABLE TblMovie(Id int, Name varchar(100), year int, date_creation date DEFAULT CURRENT_TIMESTAMP)")
